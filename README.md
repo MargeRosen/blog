8ba3032386cb721effdda4a0bb85a7e9ed7d962d
Marge Rosen <marge@masterclassu.com>
2013-08-15 15:10:14 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Blog  ##
+A simple mico app for my use on Vagrant, Chef and VirtualBox.
+
+
+Use Rails 3.2.13
+
+```
+bundle install
+rake db:migrate
+rails s
+```
+
+### License: ###
+
+MIT http://marge.mit-license.org
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '2a9c22fa95f2208288a1d13839986d7d2a3bbf70e08582391fd55a911ca2ad0d8a8f2e3fa36e080e4a98063815c0fa969f6105891b709bce0c1f8cd4e17ce06b'
